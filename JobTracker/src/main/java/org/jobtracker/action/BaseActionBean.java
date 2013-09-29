5cfa7344704a7a11a012f950313976303cb0b1bf
brianpatmarshall <brianpatmarshall@github.com>
2013-09-29 16:19:46 -0600

    New file.

---
+++ JobTracker/src/main/java/org/jobtracker/action/BaseActionBean.java
@@ -0,0 +1,22 @@
+package org.jobtracker.action;
+
+import net.sourceforge.stripes.action.ActionBean;
+import net.sourceforge.stripes.action.ActionBeanContext;
+
+/**
+ * @author SpringsTeam
+ *
+ */
+public abstract class BaseActionBean implements ActionBean
+{
+   private ActionBeanContext actionBeanContext;
+
+   public ActionBeanContext getContext()
+   {
+      return actionBeanContext;
+   }
+   public void setContext(ActionBeanContext actionBeanContext)
+   {
+      this.actionBeanContext = actionBeanContext;
+   }
+}
