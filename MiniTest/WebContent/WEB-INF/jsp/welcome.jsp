a9506e0a2fcd29f4657d22360f2bd71d467348d5
Brian Marshall <bmarshall@guidewire.com>
2014-08-03 23:38:00 -0600

    New Doc Files

--- MiniTest/WebContent/WEB-INF/jsp/welcome.jsp
+++ MiniTest/WebContent/WEB-INF/jsp/welcome.jsp
@@ -4,5 +4,6 @@
   <s:layout-component name="body">
     <h1>Welcome Ye!</h1>
     <s:link beanclass="org.cosug.action.PersonListActionBean" event="showNames">Persons</s:link>
+    <s:link beanclass="org.cosug.action.DocumentListActionBean" event="showDocuments">Documents</s:link>
   </s:layout-component>
 </s:layout-render>
--- MiniTest/src/org/cosug/util/SchemaExporter.java
+++ MiniTest/src/org/cosug/util/SchemaExporter.java
@@ -3,6 +3,7 @@ package org.cosug.util;
 
 import java.util.Properties;
 
+import org.cosug.model.Document;
 import org.cosug.model.Person;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.tool.hbm2ddl.SchemaExport;
@@ -19,15 +20,16 @@ public class SchemaExporter
       
       Properties properties = new Properties();
 
-      properties.put("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
-      properties.put("hibernate.connection.url", "jdbc:mysql://localhost:3306/osug"); 
+      properties.put("hibernate.connection.url", "jdbc:mysql://192.168.56.1:3306/osug"); 
       properties.put("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
       properties.put("hibernate.connection.username", "osug");
       properties.put("hibernate.connection.password", "feelfree");
+      properties.put("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
       properties.put("hibernate.show_sql", "true");
       config.setProperties(properties);
 
       config.addAnnotatedClass(Person.class);
+      config.addAnnotatedClass(Document.class);
 
       SchemaExport schemaExport = new SchemaExport(config);
       schemaExport.setDelimiter(";");
@@ -37,6 +39,6 @@ public class SchemaExporter
       schemaExport.setOutputFile("resource/sql/personDDL.sql");
 
       /**Just dump the schema SQLs to the console , but not execute them ***/
-      schemaExport.create(true, false);
+      schemaExport.create(true, true);
    }
 }
